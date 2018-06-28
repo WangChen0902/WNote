58237f68edc444f9c158bdf32c09552a7ae0b1e6
WangChen0902 <827913668@qq.com>
2018-06-28 14:10:36 +0800

    2018/6/28 实现显示图片的功能

--- app/src/main/java/com/example/wangchen/wnote/NewNote.java
+++ app/src/main/java/com/example/wangchen/wnote/NewNote.java
@@ -1,11 +1,17 @@
 package com.example.wangchen.wnote;
 
+import android.annotation.SuppressLint;
 import android.content.ContentResolver;
+import android.content.ContentUris;
+import android.content.Context;
 import android.content.Intent;
 import android.database.Cursor;
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.provider.DocumentsContract;
 import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
@@ -18,7 +24,10 @@ import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.Toast;
 
+import java.io.File;
 import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
@@ -32,7 +41,6 @@ public class NewNote extends AppCompatActivity {
     Datas data;
     int ids;
     String urls;
-    Uri myUri;
 
     private String INTENT_TYPE  = "image/*";
     private int REQUESTCODE = 100;
@@ -55,6 +63,13 @@ public class NewNote extends AppCompatActivity {
             data=myDatabase.getUpdate(ids);
             ed1.setText(data.getTitle());
             ed2.setText(data.getContent());
+            urls = data.getPicture();
+            File file = new File(data.getPicture());
+            if (file.exists()) {
+                Bitmap bm = BitmapFactory.decodeFile(data.getPicture());
+                //将图片显示到ImageView中
+                imageView.setImageBitmap(bm);
+            }
         }
         //保存按钮的点击事件，他和返回按钮是一样的功能，所以都调用isSave()方法；
         saveImage.setOnClickListener(new View.OnClickListener() {
@@ -99,7 +114,7 @@ public class NewNote extends AppCompatActivity {
         }
         //新建便签
         else{
-            data=new Datas(title,content,times, picture);
+            data=new Datas(title, content, times, picture);
             myDatabase.toInsert(data);
             Intent intent=new Intent(NewNote.this,MainActivity.class);
             startActivity(intent);
@@ -147,26 +162,28 @@ public class NewNote extends AppCompatActivity {
             return;
         }
 
-        //获得图片
-        Bitmap bitmap = null;
-        ContentResolver resolver = getContentResolver();
+//        //获得图片
+//        Bitmap bitmap = null;
+//        ContentResolver resolver = getContentResolver();
         if (requestCode == REQUESTCODE) {
             Uri uri = data.getData();
-            myUri = uri;
-            urls = uri.getPath();
-            try {
-                bitmap = MediaStore.Images.Media.getBitmap(resolver, uri);//获得图片
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
+            setPicture(uri);
+//            Uri uri = data.getData();
+//            myUri = uri;
+//            urls = uri.getPath();
+//            try {
+//                bitmap = MediaStore.Images.Media.getBitmap(resolver, uri);//获得图片
+//            } catch (IOException e) {
+//                e.printStackTrace();
+//            }
         }
-        imageView.setImageBitmap(bitmap);
+//        imageView.setImageBitmap(bitmap);
     }
 
     private void setPicture(Uri uri){
         Bitmap bitmap = null;
         ContentResolver resolver = getContentResolver();
-            urls = uri.getPath();
+            urls = getRealPathFromUri(this, uri);
             try {
                 bitmap = MediaStore.Images.Media.getBitmap(resolver, uri);//获得图片
             } catch (IOException e) {
@@ -174,4 +191,109 @@ public class NewNote extends AppCompatActivity {
             }
         imageView.setImageBitmap(bitmap);
     }
+
+    /**
+     * 根据Uri获取图片的绝对路径
+     *
+     * @param context 上下文对象
+     * @param uri     图片的Uri
+     * @return 如果Uri对应的图片存在, 那么返回该图片的绝对路径, 否则返回null
+     */
+    public static String getRealPathFromUri(Context context, Uri uri) {
+        int sdkVersion = Build.VERSION.SDK_INT;
+        if (sdkVersion >= 19) { // api >= 19
+            return getRealPathFromUriAboveApi19(context, uri);
+        } else { // api < 19
+            return getRealPathFromUriBelowAPI19(context, uri);
+        }
+    }
+
+    /**
+     * 适配api19以下(不包括api19),根据uri获取图片的绝对路径
+     *
+     * @param context 上下文对象
+     * @param uri     图片的Uri
+     * @return 如果Uri对应的图片存在, 那么返回该图片的绝对路径, 否则返回null
+     */
+    private static String getRealPathFromUriBelowAPI19(Context context, Uri uri) {
+        return getDataColumn(context, uri, null, null);
+    }
+
+    /**
+     * 适配api19及以上,根据uri获取图片的绝对路径
+     *
+     * @param context 上下文对象
+     * @param uri     图片的Uri
+     * @return 如果Uri对应的图片存在, 那么返回该图片的绝对路径, 否则返回null
+     */
+    @SuppressLint("NewApi")
+    private static String getRealPathFromUriAboveApi19(Context context, Uri uri) {
+        String filePath = null;
+        if (DocumentsContract.isDocumentUri(context, uri)) {
+            // 如果是document类型的 uri, 则通过document id来进行处理
+            String documentId = DocumentsContract.getDocumentId(uri);
+            if (isMediaDocument(uri)) { // MediaProvider
+                // 使用':'分割
+                String id = documentId.split(":")[1];
+
+                String selection = MediaStore.Images.Media._ID + "=?";
+                String[] selectionArgs = {id};
+                filePath = getDataColumn(context, MediaStore.Images.Media.EXTERNAL_CONTENT_URI, selection, selectionArgs);
+            } else if (isDownloadsDocument(uri)) { // DownloadsProvider
+                Uri contentUri = ContentUris.withAppendedId(Uri.parse("content://downloads/public_downloads"), Long.valueOf(documentId));
+                filePath = getDataColumn(context, contentUri, null, null);
+            }
+        } else if ("content".equalsIgnoreCase(uri.getScheme())){
+            // 如果是 content 类型的 Uri
+            filePath = getDataColumn(context, uri, null, null);
+        } else if ("file".equals(uri.getScheme())) {
+            // 如果是 file 类型的 Uri,直接获取图片对应的路径
+            filePath = uri.getPath();
+        }
+        return filePath;
+    }
+
+    /**
+     * 获取数据库表中的 _data 列，即返回Uri对应的文件路径
+     * @return
+     */
+    private static String getDataColumn(Context context, Uri uri, String selection, String[] selectionArgs) {
+        String path = null;
+
+        String[] projection = new String[]{MediaStore.Images.Media.DATA};
+        Cursor cursor = null;
+        try {
+            cursor = context.getContentResolver().query(uri, projection, selection, selectionArgs, null);
+            if (cursor != null && cursor.moveToFirst()) {
+                int columnIndex = cursor.getColumnIndexOrThrow(projection[0]);
+                path = cursor.getString(columnIndex);
+            }
+        } catch (Exception e) {
+            if (cursor != null) {
+                cursor.close();
+            }
+        }
+        return path;
+    }
+
+    /**
+     * @param uri the Uri to check
+     * @return Whether the Uri authority is MediaProvider
+     */
+    private static boolean isMediaDocument(Uri uri) {
+        return "com.android.providers.media.documents".equals(uri.getAuthority());
+    }
+
+    /**
+     * @param uri the Uri to check
+     * @return Whether the Uri authority is DownloadsProvider
+     */
+    private static boolean isDownloadsDocument(Uri uri) {
+        return "com.android.providers.downloads.documents".equals(uri.getAuthority());
+    }
+
+//    作者：tianma
+//    链接：https://www.jianshu.com/p/b168cbe50066
+//    來源：简书
+//    简书著作权归作者所有，任何形式的转载都请联系作者获得授权并注明出处。
 }
